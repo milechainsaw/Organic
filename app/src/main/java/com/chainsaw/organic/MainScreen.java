98984b9709a8a422070e7a8e83ac899aa236f5b5
Mile <mile986@gmail.com>
2015-01-15 18:38:00 +0100

    Finishing touch

--- app/src/main/java/com/chainsaw/organic/MainScreen.java
+++ app/src/main/java/com/chainsaw/organic/MainScreen.java
@@ -12,7 +12,6 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
-import android.util.Log;
 import android.view.Display;
 import android.view.View;
 import android.view.ViewTreeObserver;
